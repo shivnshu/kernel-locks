238e9422dd985c10b651e3f9d88ea10a93305961
Shivanshu Singh <shivanshusinghraj@gmail.com>
2017-10-05 17:28:48 +0530

    Add report

--- tests/analysis-test.py
+++ tests/analysis-test.py
@@ -7,18 +7,22 @@ def change_lock(lock_num):
     os.system("echo " + str(lock_num) + " > /sys/kernel/asg2_lock")
 
 def execute_and_get_time(args):
-    sum = 0
-    for i in range(3):
+    outputs = []
+    for i in range(20):
         popen = subprocess.Popen(args, stdout=subprocess.PIPE)
         popen.wait()
         output = popen.stdout.read()
         output = output.split("\n")[0].split()
-        sum += long(output[-1])
-    return sum/(i+1)
+        outputs.append(long(output[-1]))
+    mean = sum(outputs)/len(outputs)
+    outputs[:] = [abs(x-mean) for x in outputs]
+    deviation = sum(outputs)/len(outputs)
+    return (mean, deviation)
 
 def out(num_threads, total_ops, per_reads, per_writes):
     args = ("../src/syncbench", num_threads, total_ops, per_reads, per_writes)
-    print("Num_threads=%s, ops/thread=%s, per_read=%s, per_write=%s, Avg time: %d" % (num_threads, total_ops, per_reads, per_writes, execute_and_get_time(args)))
+    (mean, deviation) = execute_and_get_time(args)
+    print("Num_threads=%s, ops/thread=%s, per_read=%s, per_write=%s, Avg time: %d, Avg deviation: %d" % (num_threads, total_ops, per_reads, per_writes, mean, deviation))
 
 def analyse(lock):
     print("For " + lock + ":")
@@ -26,7 +30,7 @@ def analyse(lock):
     out("8", "50000", "99", "1")
     out("8", "50000", "95", "5")
     print("\n")
-    
+
 if __name__ == "__main__":
     change_lock(1)
     analyse("spinlock")
